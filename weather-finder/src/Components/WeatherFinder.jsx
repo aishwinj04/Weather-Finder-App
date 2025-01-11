5d63cf1f04699a7792ef20dfb8159f1a959e1853
aishwinj04 <aishwin@gmail.com>
2025-01-10 20:38:09 -0500

    Handle Input Change function

--- weather-finder/src/Components/WeatherFinder.jsx
+++ weather-finder/src/Components/WeatherFinder.jsx
@@ -7,26 +7,35 @@ import { useState } from "react"
 const WeatherFinder = () => {
     // holds api response
   const [data, setData] = useState({})
+  const [location, setLocation] = useState("")
   const apiKey = import.meta.env.VITE_API_KEY // stored in environment variable
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value)
+  }    
+  // GET request
   const search = async () => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`
     const res = await fetch(url)
     const searchData = await res.json()
     console.log(searchData)
 
     setData(searchData)
+    setLocation("")
 
   }
+
+
   return (
     <div className="container">
       <div className="weather-app">
         <div className="search">
           <div className="search-top">
             <i className="fa-solid fa-location-dot"></i>
-            <div className="location">Toronto</div>
+            <div className="location">{location}</div>
           </div>
           <div className="search-bar">
-            <input type="text" placeholder="Enter Location"/>
+            <input type="text" placeholder="Enter Location" onChange={handleInputChange}/>
             <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
           </div>
         </div>
